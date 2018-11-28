710c3b1375de2b27491f0e3bc5e575357f7edee6
unknown <jack@creativecoding4kids.com>
2018-11-28 16:30:36 -0500

    live test

--- themes/discover_western_maine_custom/single-property.php
+++ themes/discover_western_maine_custom/single-property.php
@@ -102,7 +102,6 @@
 				<div class="calendar_container">
 					<h1>Availability</h1>
 					<div id="primary_calendar" class="calendar active">
-						<object data="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=fd7ike4kdciuq9362qr02j2he0o52gc5%40import.calendar.google.com&amp;color=%231B887A&amp;src=0sf8fkp61gku81s7hdkavqa7hee1j9oq%40import.calendar.google.com&amp;color=%231B887A&amp;src=1nbrf2hqkmuarf9gcfvolkqgbh30iclu%40import.calendar.google.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></object>
 						<?php echo file_get_contents('https://calendar.google.com/calendar/embed?src=1nbrf2hqkmuarf9gcfvolkqgbh30iclu%40import.calendar.google.com&ctz=America%2FNew_York'); ?>
 					</div>
 					<div>
